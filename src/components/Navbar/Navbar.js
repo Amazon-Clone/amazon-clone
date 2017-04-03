d3df6264e29c4ff8c82807d4af0af8c15d99be9d
nturner5 <nturner5@users.noreply.github.com>
2017-04-03 12:36:58 -0600

    started navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router'
+import './Navbar.css'
 
 
 class Navbar extends Component {
     render() {
         return (
             <div>
+                
+                <section className='ad'>
+                    
+                </section>
                 <Link to='/'>Home</Link>
                 <Link to='/product'>Product</Link>
             </div>
