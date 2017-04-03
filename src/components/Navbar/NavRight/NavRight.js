8006e94b658621b0cb0f8af0884b5e7ff3e4a6f5
nturner5 <nturner5@users.noreply.github.com>
2017-04-03 17:14:27 -0600

    basic nav bar styling

---
+++ src/components/Navbar/NavRight/NavRight.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './NavRight.css'
+
+class NavRight extends Component {
+    render() {
+        return (
+          <div className="navRight">
+              Right Working;
+          </div>
+        );
+    }
+}
+
+export default NavRight;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router'
 import './Navbar.css'
+import NavRight from './NavRight/NavRight'
+import NavLeft from './NavLeft/NavLeft'
+
 
 
 class Navbar extends Component {
@@ -8,11 +11,27 @@ class Navbar extends Component {
         return (
             <div>
                 
-                <section className='ad'>
-                    
-                </section>
-                <Link to='/'>Home</Link>
-                <Link to='/product'>Product</Link>
+                <div className='ad'></div>
+                <div className='headerBlue'>
+                    <div className="navBelt">
+                        <span className='navLogo'></span>
+                        <div className='nav-search'>
+                            <div className='nav-all-dropdown'>All</div>
+                            <input type="text" value="" name="field-keywords" autocomplete="off" placeholder="" className="nav-input" tabindex="19"></input>
+                            <div className="nav-searchBtn">
+                                <div className='searchIcon'></div>
+                            </div>
+                        </div>
+                        <div>
+                            <h1 className='nav-quote'>Catch college hoops fever</h1>
+                        </div>
+                    </div>
+                    <NavLeft/>
+                    <NavRight/>
+                </div>
+                {/*{/*<Link to='/'>Home</Link>*/}
+                <Link to='/product'>Product</Link>*/}
+                
             </div>
         );
     }
