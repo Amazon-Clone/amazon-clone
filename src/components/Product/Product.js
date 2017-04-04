4aaeed18af1fde60e22277b811e66a51d9e79e1c
Emiliano Mesquita <emiliano_mesquita@hotmail.com>
2017-04-04 12:08:11 -0600

    Added sub-nav in products view, added status-nav in products view

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+import './Product.css';
 
 class Product extends Component {
     render() {
         return (
-            <div>
-                <h1> This is Products Page</h1>
-            </div>
+            <div className='productContainer'>
+                <div className='smallNav'>
+                    <Link to='#' className='categoryParent'>Home Audio &amp; Theater</Link>
+                    <Link to='#' className='categoryGroup'>Premium Audio</Link>
+                    <Link to='#' className='categoryGroup'>Headphones</Link>
+                    <Link to='#' className='categoryGroup'>Home Theatre Systems</Link>
+                    <Link to='#' className='categoryGroup'>Speakers</Link>
+                    <Link to='#' className='categoryGroup'>Wireless Audio</Link>
+                    <Link to='#' className='categoryGroup'>Stereo Systems Components</Link>
+                    <Link to='#' className='categoryGroup'>Accesories</Link>
+                    <Link to='#' className='categoryGroup'>Deals</Link>
+                </div>
+                <div className="statusBar">
+                    <p>Displaying <b>status</b> information</p>
+                </div>
+                <div className="mainBody">
+
+                </div>
+            </div >
         );
     }
 }
