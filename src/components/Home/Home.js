4135abfccd6a5640984f7faab4146d29652be62d
Jacob Hamblin <jacobhamblin83@gmail.com>
2017-04-04 17:00:41 -0600

    end of tuesdays work on home view

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,6 +5,7 @@ import Carousel from './Carousel';
 import HomeSignIn from './Home_sign_in';
 import PopularDepartments from './Popular_Departments';
 import TheDressShop from './The_dress_shop.js'
+import LastSection from './Last_Section'
 
 class Home extends Component {
     render() {
@@ -17,7 +18,7 @@ class Home extends Component {
                     <HomeSignIn />
                     <PopularDepartments />
                     <TheDressShop />
-                    <PopularDepartments />
+                    <LastSection />
                 </div>
                 
                     {/*<div className='home-popular-departements'>
