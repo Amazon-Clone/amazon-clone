a61711b5cd3954245a13922f93e42a1d6d9d85d8
Sang Ahn <sangchunahn@users.noreply.github.com>
2017-04-05 14:48:17 -0600

    reconfigured footerlanguage folder for detection

---
+++ src/components/Footer/FooterLanguage/FooterLanguage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './FooterLanguage.css'
+
+class FooterLanguage extends Component {
+    render() {
+        return (
+            <div className='footer-language-container'>
+                <div className='footer-language-box'>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default FooterLanguage;
