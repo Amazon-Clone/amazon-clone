229d597c5ab5997ce0fc3001e20ad3a09301af3c
Sang Ahn <sangchunahn@users.noreply.github.com>
2017-04-06 16:39:58 -0600

    get request successful from the backend to front for user

--- src/actions/get_user.js
+++ src/actions/get_user.js
@@ -4,9 +4,7 @@ import axios from 'axios'
 export const GET_USER = 'GET_USER'
 
 export function getUser() {
-    const request = axios.get(`/api/user`).then((response)=> {
-        console.log("back: ", response.data)
-    })
+    const request = axios.get(`/api/user`)
     return {
         type: GET_USER,
         payload: request
--- src/reducers/user_reducer.js
+++ src/reducers/user_reducer.js
@@ -1,10 +1,10 @@
 import { GET_USER } from '../actions/get_user'
 
-const INITIAL_STATE = {all: [], post: null };
+const INITIAL_STATE = {users: [], post: null };
 export default function(state = INITIAL_STATE, action) {
     switch(action.type) {
         case GET_USER:
-            return {...state, all: action.payload}
+            return {...state, users: action.payload.data}
         default:
             return state;
     }
