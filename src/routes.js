8f75c20513e375d8c6afbf74b610e93ebd1587e4
nturner5 <nturner5@users.noreply.github.com>
2017-04-07 15:31:06 -0600

    started making productdetails route

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import Product from './components/Product/Product'
 import ProductTest from './components/ProductTest/ProductTest'
 import Register from './components/Register/Register'
 import Login from './components/Login/Login'
+import ProductDetails from './components/ProductDetails/ProductDetails'
 
 export default(
     <Route>
@@ -15,6 +16,7 @@ export default(
         <IndexRoute component={ Home }/>
         <Route path='/product' component={ Product }/>
         <Route path='/producttest' component={ ProductTest }/>
+        <Route path='/product/:id' component={ ProductDetails }/>
         
     </Route>
     <Route path='/register' component={ Register }/>
