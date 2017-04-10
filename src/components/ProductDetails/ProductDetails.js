30580f1003c8ef3a60c711586ee0611f76f9cb8d
nturner5 <nturner5@users.noreply.github.com>
2017-04-10 10:09:08 -0600

    started cart

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { addToCart } from '/../../actions/cart'
 
 class ProductDetails extends Component {
-
+constructor(props){
+    super(props)
+    this.state = { 
+        productid: 5
+    }
+}
+submitAdd = function(productid){
+    addToCart(productid)
+}
     render() {
+        
+        functi
         return (
+            
             <div>
                 ProductDetails working
+                <button type="" onclick={this.submitAdd}></button>
             </div>
         );
     }
