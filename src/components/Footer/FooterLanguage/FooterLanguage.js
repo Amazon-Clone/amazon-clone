da6d5b1b79d62b2b0d4331d7faaf039007480ad5
Sang Ahn <sangchunahn@users.noreply.github.com>
2017-04-11 16:57:47 -0600

    css for language and country selector added, in progress

--- src/components/Footer/FooterLanguage/FooterLanguage.js
+++ src/components/Footer/FooterLanguage/FooterLanguage.js
@@ -7,9 +7,15 @@ class FooterLanguage extends Component {
             <div className='footer-language-container'>
                 <div className='footer-language-box'>
                     <div className='footer-center-box'>
-                        <div className='footer-amazon-logo'></div>
-                        <a href='#' className='footer-language-selector'></a>
-                        <a href='#' className='footer-country-selector'></a>
+                        <div className='footer-language-logo'></div>
+                        <div className='footer-right-center'>
+                            <a href='#' className='footer-language-selector'>
+                                <div className='footer-globe'>
+
+                                </div>
+                            </a>
+                            <a href='#' className='footer-country-selector'></a>
+                        </div>
                     </div>
                 </div>
             </div>
