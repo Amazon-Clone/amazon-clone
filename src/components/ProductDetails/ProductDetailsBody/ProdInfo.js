8e7413d502030a640f6e45af1a40e48baa6b979c
nturner5 <nturner5@users.noreply.github.com>
2017-04-12 15:08:57 -0600

    more styling

--- src/components/ProductDetails/ProductDetailsBody/ProdInfo.js
+++ src/components/ProductDetails/ProductDetailsBody/ProdInfo.js
@@ -18,7 +18,7 @@ class ProdInfo extends Component {
                     <a href="" className='blueATag'> 277 answered questions</a>
                 </div>
                 <h2 className='otherSellers'>Price <a className='priceRed'> $71.00</a></h2>
-                <div className='flexOnly'>
+                <div className='flexOnly pad'>
                     <h2 className='sizeGrey'>Size: </h2>
                     <h2 className='sizeBlack'>65-inch</h2>
                 </div>
@@ -32,11 +32,11 @@ class ProdInfo extends Component {
                         <h2 className='boxPrice'>$399.00</h2>
                     </div>
                 </div>
-                <div className='flexOnly'>
+                <div className='flexOnly pad'>
                     <h2 className='sizeGrey'>Style: </h2>
                     <h2 className='sizeBlack'>TV</h2>
                 </div>
-                <ul>
+                <ul className='pad'>
                     <li className='listBullet'>Smart Functionality: Yes - web OS 3.0</li>
                     <li className='listBullet'>Dimensions (W x H x D): TV without stand: 57.1" x 33.0" x 1.9", TV with stand: 57.1" x 34.7" x 8.9"</li>
                     <li className='listBullet'>Inputs: 4 HMDI, 3 USB, 1 RF, 1 Component, 1 Composite, 1 Optical, 1 RS232C, 1 Ethernet</li>
