dbbe888221a8165caf6593eae9217884c75d8fd9
nturner5 <nturner5@users.noreply.github.com>
2017-04-12 09:26:05 -0600

    made basic product details view

--- src/components/ProductDetails/ProductDetailsBody/ProdInfo.js
+++ src/components/ProductDetails/ProductDetailsBody/ProdInfo.js
@@ -18,7 +18,42 @@ class ProdInfo extends Component {
                     <a href="" className='blueATag'> 277 answered questions</a>
                 </div>
                 <h2 className='otherSellers'>Available from <a className='blueATag'> these sellers.</a></h2>
-                <h2 className='sizeParent'>Size: <h2 className='sizeChild'>65-inch</h2></h2>
+                <div className='flexOnly'>
+                    <h2 className='sizeGrey'>Size: </h2>
+                    <h2 className='sizeBlack'>65-inch</h2>
+                </div>
+                <div className='sizeBoxParent'>
+                    <div className='sizeBoxChild'>
+                        <h2>55-inch</h2>
+                        <h2 className='boxPrice'>$1997.0</h2>
+                    </div>
+                    <div className='sizeBoxChild'>
+                         <h2>65-inch</h2>
+                        <h2 className='boxPrice'>$399.00</h2>
+                    </div>
+                </div>
+                <div className='flexOnly'>
+                    <h2 className='sizeGrey'>Style: </h2>
+                    <h2 className='sizeBlack'>TV</h2>
+                </div>
+                <ul>
+                    <li className='listBullet'>Smart Functionality: Yes - web OS 3.0</li>
+                    <li className='listBullet'>Dimensions (W x H x D): TV without stand: 57.1" x 33.0" x 1.9", TV with stand: 57.1" x 34.7" x 8.9"</li>
+                    <li className='listBullet'>Inputs: 4 HMDI, 3 USB, 1 RF, 1 Component, 1 Composite, 1 Optical, 1 RS232C, 1 Ethernet</li>
+                    <li className='listBullet'>HDR Enhanced Display</li>
+                </ul>
+                <a href='' className='blueATag'>See more product details</a>
+
+               
+                 <a className='blueATag'>Used & new (39) from $399.00 + $41.49 shipping </a>
+                    
+                <div className='flexOnly'>
+                    <div className='messageIcon'></div>
+
+                   <a className='blueATag'> Report incorrect product information.</a>
+                </div>
+                <h2>This item’s packaging will indicate what is inside and cannot be hidden.</h2>
+
             </div>
         );
     }
