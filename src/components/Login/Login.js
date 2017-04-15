a1b11ef06dfb343fd855ead6bdd3d4738bda23b3
Sang Ahn <sangchunahn@users.noreply.github.com>
2017-04-14 22:43:19 -0600

    .then breaking code in login function

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
 import LoginForm from './LoginForm'
 import { login } from '../../actions/login'
+import { getUser} from '../../actions/get_user'
+import { connect } from 'react-redux'
 
 
 class Login extends Component {
+    componentWillMount() {
+        this.props.getUser()
+    }
     // submit = (values) => {
     //     console.log(values);
     //     register(value)
     // }
     submit(values) {
         console.log(values)
-        login(values)
+        login(values) 
+        if (values === this.props.user) {
+            window.location.href = '#/'
+        } else 
+        return alert('Wrong Username or Password')
+
     }
     render() {
         return (
@@ -18,5 +28,8 @@ class Login extends Component {
         )
     }
 }
+function mapStateToProps(state) {
+    user: state.user.all
+}
 
-export default Login
+export default connect(mapStateToProps, { getUser })(Login)
