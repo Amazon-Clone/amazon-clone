6fa66ba77642c3f8a72229da83f3e647a89bb3de
Sang Ahn <sangchunahn@users.noreply.github.com>
2017-04-17 09:50:36 -0600

    login redirect functionality, cannot get user from database

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react'
 import LoginForm from './LoginForm'
 import { login } from '../../actions/login'
+import { getUser } from '../../actions/get_user' 
+import {connect } from 'react-redux'
 
 
 class Login extends Component {
+    constructor(props) {
+        super(props)
+    }
+    componentWillMount() {
+        this.props.getUser().then((use)=>{
+            this.setState({user: user.data})
+        })
+    }
     // submit = (values) => {
     //     console.log(values);
     //     register(value)
     // }
     submit(values) {
-        console.log(values)
         login(values)
+        if( values === user[0]) {
+            return window.location.href = '#'
+        }   else {
+            return alert('Incorrect Username or Password')
+        }
+        console.log(values)
+
     }
     render() {
         return (
@@ -18,5 +34,9 @@ class Login extends Component {
         )
     }
 }
-
-export default Login
+function mapStateToProps(state) {
+    return {
+        user: state.user.all
+    }
+}
+export default connect(mapStateToProps, { getUser }) (Login)
