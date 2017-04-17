4f3ce43fcc448f5530d15fe4ff7357d979e0278b
nturner5 <nturner5@users.noreply.github.com>
2017-04-17 09:37:46 -0600

    fixed keys file

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
-var stripeSecretKey = 'sk_test_kZzzOMg8ZgcslrfKWyXyZk1x';
-var sessionSecretKey = '_VuT89**::;,<<KaO(Ja@1)Jx1@&^^$#';
-
-module.exports = {
-  stripeSecretKey: stripeSecretKey,
-  sessionSecretKey: sessionSecretKey,
-  localuserkey: 'postgres://postgres:Testies1-1@localhost/front2back'
+var stripeSecretKey = 'sk_test_kZzzOMg8ZgcslrfKWyXyZk1x';        
+var sessionSecretKey = '_VuT89**::;,<<KaO(Ja@1)Jx1@&^^$#';        
+        
+module.exports = {        
+    stripeSecretKey: stripeSecretKey,        
+    sessionSecretKey: sessionSecretKey,        
+    localuserkey: "postgres://postgres@localhost/amazon-clone"        
 };
