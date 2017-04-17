d9c95e66f7405444b91d8acb9e5c3e2faa4b6f7d
nturner5 <nturner5@users.noreply.github.com>
2017-04-17 09:36:59 -0600

    Delete keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-var stripeSecretKey = 'sk_test_kZzzOMg8ZgcslrfKWyXyZk1x';
-var sessionSecretKey = '_VuT89**::;,<<KaO(Ja@1)Jx1@&^^$#';
-
-module.exports = {
-  stripeSecretKey: stripeSecretKey,
-  sessionSecretKey: sessionSecretKey,
-  localuserkey: 'postgres://postgres:Testies1-1@localhost/front2back'
-};
