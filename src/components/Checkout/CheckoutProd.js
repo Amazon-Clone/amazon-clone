3b80e9bacac70974c2c942d97cb87a1ae7dd2b76
Sang Ahn <sangchunahn@users.noreply.github.com>
2017-04-19 15:08:27 -0600

    finished product content on checkout view

--- src/components/Checkout/CheckoutProd.js
+++ src/components/Checkout/CheckoutProd.js
@@ -12,9 +12,20 @@ class CheckoutProd extends Component {
                 <div className='checkout-product-bottom'>
                     <div className='checkout-product-left'>
                         <img src="/images/checkout-images/mac.jpg" alt=""/>
-                        <div className='checkout-product-content'></div>
+                        <div className='checkout-product-content'>
+                            <h2>Apple MacBook Pro MLW72LL/A 15.4-inch Laptop with Touch Bar (2.6GHz quad-core Intel Core i7, 256GB Retina Display), Silver</h2>
+                            <div className='checkout-cost'>$2,249.00</div>
+                            <p>Amazon Prime eligible<a href='#'>Join now</a></p>
+                            <div className='checkout-stock'>Only 17 left in stock (more on the way).</div>
+                            <h3>Quantity: 1<a href='#'>Change</a></h3>
+                            <p className='checkout-sold-by'>Sold by: Amazon.com LLC</p>
+                            <button><img src="/images/checkout-images/gift_btn.png" alt=""/><p>Add a gift receipt</p></button>
+                            <div className='other-gift-option'> and see other gift options</div>
+                        </div>
                     </div>
-                    <div className='checkout-product-right'></div>
+                    <div className='checkout-product-right'>
+                        <h1>Choose a delivery option:</h1>
+                    </div>
                 </div>
             </div>
         );
