d85aa5818b0020c987ef3156810f840cd8e860b6
Sang Ahn <sangchunahn@users.noreply.github.com>
2017-04-19 14:27:13 -0600

    added the images and the content for product in checkout

--- src/components/Checkout/CheckoutProd.js
+++ src/components/Checkout/CheckoutProd.js
@@ -4,8 +4,18 @@ import './CheckoutProd.css'
 class CheckoutProd extends Component {
     render() {
         return (
-            <div>
-                
+            <div className='checkout-product-container'>
+                <div className='checkout-delivery-box'>
+                    <h1>Guaranteed delivery: April 21, 2017</h1>
+                    <p>If you order in the next 5 hours and 30 minutes</p> <span>(<a href='#'>Details</a>)</span>
+                </div>
+                <div className='checkout-product-bottom'>
+                    <div className='checkout-product-left'>
+                        <img src="/images/checkout-images/mac.jpg" alt=""/>
+                        <div className='checkout-product-content'></div>
+                    </div>
+                    <div className='checkout-product-right'></div>
+                </div>
             </div>
         );
     }
